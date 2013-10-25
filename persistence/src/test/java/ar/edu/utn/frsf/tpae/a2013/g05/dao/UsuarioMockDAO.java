314005fb39cfe192fd0ec3843f0b16f3af4fc8df
dariodefilippis@gmail.com <dariodefilippis@gmail.com@2e10e436-2170-01be-8607-0d093d39adc9>
2013-10-25 01:27:54 +0000

    Se crearon los Factory de todos los DAO. Se crearon los Mock de todos los DAO. Faltan hacer las implementaciones correctas de los mocks.

---
+++ persistence/src/test/java/ar/edu/utn/frsf/tpae/a2013/g05/dao/UsuarioMockDAO.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.frsf.tpae.a2013.g05.dao;
+
+import ar.edu.utn.frsf.tpae.a2013.g05.model.Usuario;
+
+
+/**
+ * @author root
+ *
+ */
+public class UsuarioMockDAO implements UsuarioDAO{
+
+	@Override
+	public Usuario validarUsuario(String usuario, String password) {
+		// TODO Crear el mock para validar un usuario
+		return null;
+	}
+
+}
