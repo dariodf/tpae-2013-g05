83c6cb28d1604b7168acd75caab3cc4e39fb6e26
dariodefilippis@gmail.com <dariodefilippis@gmail.com@2e10e436-2170-01be-8607-0d093d39adc9>
2013-10-31 20:17:32 +0000

    TP-07: Terminados los casos de Test generales.

--- persistence/src/test/java/ar/edu/utn/frsf/tpae/a2013/g05/dao/junits/UsuarioDAOTest.java
+++ persistence/src/test/java/ar/edu/utn/frsf/tpae/a2013/g05/dao/junits/UsuarioDAOTest.java
@@ -40,28 +40,25 @@ public class UsuarioDAOTest {
 		Usuario usuario = new Empleado(1, "Agustin","1234", "Martinez",
 				"Agustin", "32123456", "Programador Java Junior", "20321234568",
 				"123456789012345678901234567890", "156123456", "mrtnz.agustin@gmail.com");
-		
 		Usuario usuarioRetornado = usuarioDAO.validarUsuario("Agustin","1234");
-		// Probamos que el Id sea el correcto.
-		assertEquals(usuarioRetornado.getId(), usuario.getId());
+		assertEquals(usuarioRetornado.getId(), usuario.getId()); // Probamos que el Id sea el correcto.
 		
 		
 		usuarioRetornado = usuarioDAO.validarUsuario("Carlos","1234");
-		// Probamos que la validación no es correcta, por lo tanto retorna null.
-		assertEquals(usuarioRetornado, null);
+		assertEquals(usuarioRetornado, null); // Probamos que la validación no es correcta, por lo tanto retorna null.
+		
 		
 		usuarioRetornado = usuarioDAO.validarUsuario("Dario","5678");
 		usuario = new Supervisor(3, "Dario", "5678", "De Filippis",
 				"Dario", "30789456", new Date());
-		// Probamos que traiga un Supervisor.
-		assertEquals(usuario.getClass(), usuarioRetornado.getClass());	
+		assertEquals(usuario.getClass(), usuarioRetornado.getClass()); // Probamos que traiga un Supervisor.	
+		
 		
 		usuarioRetornado = usuarioDAO.validarUsuario("Daniela","3456");
 		usuario = new Empleado(2, "Daniela","3456", "Greca",
 				"Daniela", "31654321", "Programador Java Senior", "28316543218",
 				"098765432109876543210987654321", "156654321", "daniela.greca@gmail.com");
-		// Probamos que traiga un Empleado.
-		assertEquals(usuario.getClass(), usuarioRetornado.getClass());
+		assertEquals(usuario.getClass(), usuarioRetornado.getClass()); // Probamos que traiga un Empleado.
 		
 	}
 
--- persistence/src/test/java/ar/edu/utn/frsf/tpae/a2013/g05/dao/mocks/GastoMockDAO.java
+++ persistence/src/test/java/ar/edu/utn/frsf/tpae/a2013/g05/dao/mocks/GastoMockDAO.java
@@ -20,7 +20,7 @@ import ar.edu.utn.frsf.tpae.a2013.g05.model.Supervisor;
  */
 public class GastoMockDAO implements GastoDAO{
 
-	boolean dbCaida = true;
+	boolean dbCaida = false;
 	@Override
 	public Gasto persistir(Gasto gasto) {
 		if (dbCaida)
@@ -46,7 +46,7 @@ public class GastoMockDAO implements GastoDAO{
 				"Dario", "30789456", new Date()),new Empleado(1, "Agustin","1234", "Martinez",
 						"Agustin", "32123456", "Programador Java Junior", "20321234568",
 						"123456789012345678901234567890", "156123456", "mrtnz.agustin@gmail.com")), new Date(), (float) 1200.99, "B031");
-		Gasto gastoCuatro = new Gasto(3, new SolicitudDeGastos(5, new CentroDeCosto(1, "Centro Uno"), "Gastos en viaticos a CBA", 12354, "No Procesada", new Supervisor(3, "Dario", "5678", "De Filippis",
+		Gasto gastoCuatro = new Gasto(4, new SolicitudDeGastos(5, new CentroDeCosto(1, "Centro Uno"), "Gastos en viaticos a CBA", 12354, "No Procesada", new Supervisor(3, "Dario", "5678", "De Filippis",
 				"Dario", "30789456", new Date()),new Empleado(2, "Daniela","3456", "Greca",
 						"Daniela", "31654321", "Programador Java Senior", "28316543218",
 						"098765432109876543210987654321", "156654321", "daniela.greca@gmail.com")), new Date(), (float) 4120.99, "B068");
@@ -62,21 +62,21 @@ public class GastoMockDAO implements GastoDAO{
 			listaDeGastos.add(gastoCuatro);
 			return listaDeGastos;
 		}
-		else if (centrosDeCosto.get(0).getId() == 1 && empleados.size()== 0)
+		else if (centrosDeCosto.size()!=0 && centrosDeCosto.get(0).getId() == 1 && empleados.size()== 0)
 		{
 			listaDeGastos.add(gastoUno);
 			listaDeGastos.add(gastoTres);
 			listaDeGastos.add(gastoCuatro);
 			return listaDeGastos;
 		}
-		else if(centrosDeCosto.size()==0 && empleados.get(0).getId() == 1)
+		else if(centrosDeCosto.size()==0 && empleados.size()!= 0 &&  empleados.get(0).getId() == 1)
 		{
 			listaDeGastos.add(gastoUno);
 			listaDeGastos.add(gastoDos);
 			listaDeGastos.add(gastoTres);
 			return listaDeGastos;
 		}
-		else if(centrosDeCosto.get(0).getId() == 1 && empleados.get(0).getId() == 2){
+		else if(centrosDeCosto.get(0).getId() == 1 &&  empleados.get(0).getId() == 2){
 			listaDeGastos.add(gastoCuatro);
 			return listaDeGastos;
 		}
