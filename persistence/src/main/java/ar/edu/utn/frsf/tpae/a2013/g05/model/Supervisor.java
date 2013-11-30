4bbeea18e00648fb18d9759dd5895ed7cf0c500c
daniela.greca@gmail.com <daniela.greca@gmail.com@2e10e436-2170-01be-8607-0d093d39adc9>
2013-11-30 23:59:31 +0000

    TP-10: Eliminación de archivos que ya no sirven en esta etapa. Agregado de la carpeta resources, donde deben ir los archivos de recursos. Agregado del Login Bean y el servicio de Usuario.

--- persistence/src/main/java/ar/edu/utn/frsf/tpae/a2013/g05/model/Supervisor.java
+++ persistence/src/main/java/ar/edu/utn/frsf/tpae/a2013/g05/model/Supervisor.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Supervisor extends Usuario {
 
 	@Column(name = "spv_ult_acc")
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date ultimoAcceso;
 
 	/**
---
+++ service/src/main/java/ar/edu/utn/frsf/tpae/a2013/g05/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frsf.tpae.a2013.g05.service.impl;
+
+import ar.edu.utn.frsf.tpae.a2013.g05.model.Usuario;
+import ar.edu.utn.frsf.tpae.a2013.g05.service.UsuarioService;
+
+/**
+ * Implementación del servicio para usuario.
+ * 
+ * @author Daniela
+ */
+public class UsuarioServiceImpl implements UsuarioService {
+
+	@Override
+	public Usuario ingresar(String usuario, String password) {
+		return null;
+	}
+
+
+}
