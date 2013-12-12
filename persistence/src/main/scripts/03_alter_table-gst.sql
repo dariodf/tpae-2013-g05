ac46aedefc6a76e1ab145aa818e5ec4cb3bd0dd1
mrtnz.agustin@gmail.com <mrtnz.agustin@gmail.com@2e10e436-2170-01be-8607-0d093d39adc9>
2013-12-12 18:59:10 +0000

    TP-10: CU finalizados y probados. Versión integral completa del sistema. Corrección de scripts de creación de tablas.

--- persistence/src/main/scripts/03_alter_table-gst.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE gst ADD gst_cmt_emp character varying(256);
-ALTER TABLE gst ADD gst_fech_real timestamp;
-ALTER TABLE gst RENAME gst_fech to gst_fech_alta;
--- persistence/src/main/scripts/05_insert-test-data.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM cco;
-
-INSERT INTO cco (cco_nom) VALUES
-('Centro Uno'),
-('Centro Dos');
